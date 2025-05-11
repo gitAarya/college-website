847d4c204f059d542207b0b88716a06b0c6adc81
gitAarya <aaryagithub@gmail.com>
2025-05-11 21:48:35 +0545

    Add Swiper integration and update styles for header and navigation
    - Integrated Swiper for image slides in the main section.
    - Updated header to be fixed with improved navigation styles.
    - Added new images for logo and background.
    - Enhanced CSS with new utility classes for better layout control.

--- src/main.js
+++ src/main.js
@@ -1,23 +1,29 @@
 // import './style.css'
 import "./output.css";
+// import Swiper from 'swiper/bundle';
+// import 'swiper/css/bundle';
 
 const menu = document.getElementById("main-menu");
 const nav = document.getElementById("ulNav");
 // const navLi=document.getElementsByClassName('list-item')
 const programs = document.getElementById("programList");
-const programs1 = document.getElementById("programList1");
+const programsIcon = document.getElementById("programListIcon");
 
 
 const programList = document.getElementById("list");
-
 const toggleBtn = document.getElementById("toggleMenu");
+
+// displaying menu option on smaller screen
 toggleBtn.addEventListener("click", (e) => {
   // Toggle between 'menu' and 'close' icons
   e.target.name =
     e.target.name === "menu-outline" ? "close-outline" : "menu-outline";
 });
+
+//showing the header menu on mobile screeen
 toggleBtn.addEventListener("click", function () {
   menu.classList.toggle("hidden");
+  menu.classList.toggle("text-black");
   menu.classList.toggle("absolute");
   menu.classList.toggle("bg-white");
   menu.classList.toggle("w-full");
@@ -27,15 +33,30 @@ toggleBtn.addEventListener("click", function () {
   menu.classList.toggle("py-8");
   menu.classList.toggle("flex-col");
   nav.classList.toggle("flex-col");
-  navLi.classList.toggle('border-[2px]')
-
-  console.log("clicked");
 });
 
+//showin the programs 
 programs.addEventListener("click", () => {
   programList.classList.toggle("hidden");
 });
-programs1.addEventListener("click", () => {
+programsIcon.addEventListener("click", () => {
   programList.classList.toggle("hidden");
 });
 
+// -------------------
+//displaying teh slides 
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 1,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
+window.addEventListener('click',()=>{
+  document.getElementById('nav').classList.toggle("bg-black")
+  console.log("clicked");
+  
+})
